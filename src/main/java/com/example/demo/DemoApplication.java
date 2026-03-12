d3687c8a3685ae126b8884a2896dc713b80e5b2f
phxbinh <phxbinh@gmail.com>
2026-03-12 11:55:36 +0700

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,24 @@
+
+// src/main/java/com/example/demo/DemoApplication.java
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Đây là các dòng import đầy đủ bạn cần:
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // Thêm cái này để Spring biết class này có chứa API
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+    // Thêm hàm này để khi vào trang chủ nó hiện chữ
+    @GetMapping("/")
+    public String hello() {
+        return "Chào bạn! API này chạy từ iPhone của bạn thông qua Codespaces nhé!";
+    }
 }
